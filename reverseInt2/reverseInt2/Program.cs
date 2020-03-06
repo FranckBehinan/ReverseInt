7b39bb8745ecd6c2c5fac8be0771b16c5f006ca3
Franck Behinan <59900652+FranckBehinan@users.noreply.github.com>
2020-03-05 17:50:14 -0800

    commit

--- reverseInt2/reverseInt2/Program.cs
+++ reverseInt2/reverseInt2/Program.cs
@@ -19,7 +19,6 @@ namespace reverseInt2
                 Reverse = (Reverse * 10) + remainder;
                 Number = Number / 10;
             }
-            Console.WriteLine();
             Console.WriteLine("Reverse No. is {0}", Reverse);
             Console.ReadLine();
         }
